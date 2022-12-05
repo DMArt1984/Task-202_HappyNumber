170a69f4ecbb91d6054362a81d8a5808aba53760
DMArt1984 <dmxart13@gmail.com>
2022-12-05 15:08:23 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _202HappyNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 202");
+            Console.WriteLine(IsHappy(19));
+            Console.WriteLine(IsHappy(2));
+            Console.ReadKey();
+        }
+
+        // https://leetcode.com/problems/happy-number/
+        // 202. Happy Number
+        // Write an algorithm to determine if a number n is happy.
+        // A happy number is a number defined by the following process:
+        // Starting with any positive integer, replace the number by the sum of the squares of its digits.
+        // Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+        // Those numbers for which this process ends in 1 are happy.
+        // Return true if n is a happy number, and false if not.
+
+        static public bool IsHappy(int n)
+        {
+            Console.WriteLine($"n = {n}");
+            List<double> used = new List<double>();
+            double num = n;
+            while (num != 1 && !used.Contains(num)) {
+                used.Add(num);
+                string s = num.ToString();
+                num = 0;
+                for (int i=0; i < s.Length; i++)
+                {
+                    num += Math.Pow(int.Parse(s[i].ToString()), 2);
+                    Console.Write($" {s[i]} > {num}; ");
+                }
+                Console.WriteLine($" = {num}");
+            }
+            return num == 1;
+        }
+    }
+}
